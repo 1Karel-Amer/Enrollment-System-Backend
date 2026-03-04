44995799c51136b93b3f33d176cda7473dfc05a9
1Karel-Amer <princesskarelamer@gmail.com>
2026-03-04 11:16:38 +0800

    added HasApiTokens to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Models;
+namespace App\Http\Controllers\Api;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use App\Htpp\Controllers\Controller;
+use Illuminate\http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class AuthController extends Controller
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
